04f725f697f74780b3541e5e70c79120cb872b8f
AnitaPiket <anitajongbloed@icloud.com>
2025-05-02 18:25:20 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,24 @@ self.addEventListener('install', (event) => {
       ]);
     })
   );
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== 'pwa-cache') {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
