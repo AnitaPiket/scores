4575492d77e678d4db5c8399ef1ececafc003d76
AnitaPiket <anitajongbloed@icloud.com>
2025-05-10 12:17:00 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -94,22 +94,21 @@ function updateChallengeHighlight(team) {
   const max = Math.max(...scores);
   const min = Math.min(...scores);
 
-  values.forEach(v => {
-    v.el.classList.remove('highlight');
-    v.el.style.backgroundColor = ''; // reset
+  const allSame = scores.every(v => v === max) && max !== 0;
 
-    if (v.value === max && max === min) {
-      // alles gelijk
-      v.el.style.backgroundColor = '#FFD700'; // geel
-    } else if (v.value === max) {
-      v.el.style.backgroundColor = '#FF4D4D'; // rood
-    } else if (v.value === min) {
-      v.el.style.backgroundColor = '#4CAF50'; // groen
-    } else {
-      v.el.style.backgroundColor = ''; // neutraal
+  values.forEach(v => {
+    v.el.style.backgroundColor = "";
+
+    if (allSame) {
+      v.el.style.backgroundColor = "#FFD700"; // geel
+    } else if (v.value === max && max !== min) {
+      v.el.style.backgroundColor = "#FF4D4D"; // rood
+    } else if (v.value === min && max !== min) {
+      v.el.style.backgroundColor = "#4CAF50"; // groen
     }
   });
 }
+
 
 function showGame(nr) {
   for (let i = 1; i <= 4; i++) {
